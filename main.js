fb38e34bbf69690cf147d33970d986488b378d84
Rorove87 <91465248+Rorove87@users.noreply.github.com>
2022-02-28 15:19:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Cookie Banner
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".close-btn");
+
+cookieButton.addEventListener("click", () => {
+        cookieContainer.classList.remove("active");
+        localStorage.setItem("cookieBannerDisplayed", "true")
+});
+
+
+setTimeout(() => {
+        if(!localStorage.getItem("cookieBannerDisplayed")) {
+                cookieContainer.classList.add("active");
+        }
+}, 2000);
